e2bfa35005fd035834ce6f506cab74b036f7605c
Aldi2801 <94529473+Aldi2801@users.noreply.github.com>
2022-01-05 12:27:37 +0700

    init

---
+++ administrator/logs/error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-01-01 15:34:29 UTC
+#Software: Joomla! 4.0.5 Stable [ Furaha ] 11-December-2021 20:52 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-01-01T15:34:29+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2022-01-02T01:31:36+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2022-01-03T04:33:41+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2022-01-05T05:08:27+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2022-01-05T05:08:42+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2022-01-05T05:08:57+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
